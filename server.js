216d2c61bf69496c0aeccb120c66b586f50fe68f
Kiwane Garner <kiwanegarner@Kiwanes-MacBook-Pro.local>
2018-11-21 15:26:29 -0500

    made changes to the server.js file for hosting

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
-
-
 const express = require("express");
-//const serveStatic = require("serve-static");
+const serveStatic = require("serve-static");
 const path = require("path");
 const port = process.env.PORT || 5000;
 const app = express();
 
-app.use(express.static(__dirname));
-//app.use("/", servestatic ( path.join (__dirname, "/public")))
+//app.use(express.static(__dirname));
+app.use("/", serveStatic ( path.join (__dirname, "/public")))
 
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "index.html"));
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname + "/public/index.html"));
 });
 
 app.listen(port);
